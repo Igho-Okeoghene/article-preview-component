ac3df67b6fd1f5081cc69f9222dcac7f05b3cee5
Okeoghene Eunice Igho <61965289+Igho-Okeoghene@users.noreply.github.com>
2023-02-03 13:13:51 +0100

    desktop view and JS toggle function update

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 //make section-three display:none and shareSection display:flex through JavaScript.
 //reverse previous action after 5 seconds;
     document.getElementById("shareBtn").addEventListener('click', function(){
-        document.getElementById("thirdSection").style.display = 'none';
-        document.getElementById("shareSection").style.display = 'flex';
-        console.log("Switched Change");
+        let btn = document.getElementById("thirdSection");
+        let popup = document.getElementById("shareSection");
+        btn.classList.toggle("no-display");
+        popup.classList.toggle("display");
+         console.log("Switched Change");
         setTimeout(() => {
-            document.getElementById("shareSection").style.display = 'none';
-            document.getElementById("thirdSection").style.display = 'flex';
-            console.log("Initial Design Restored");
+            btn.classList.remove("no-display");
+            popup.classList.remove("display");console.log("Initial Design Restored");
              }, "5000")
         
     });
