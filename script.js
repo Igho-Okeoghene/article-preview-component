c8a1c08834ad535ce4d8ff2b8bec87583c47361c
Okeoghene Eunice Igho <61965289+Igho-Okeoghene@users.noreply.github.com>
2023-02-10 23:07:37 +0100

    final updates

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 //listen to when the btn is clicked
 //make section-three display:none and shareSection display:flex through JavaScript.
 //reverse previous action after 5 seconds;
-    document.getElementById("shareBtn").addEventListener('click', function(){
+
+
+
+    document.getElementById("shareBtn").addEventListener('click', function(e){
         let btn = document.getElementById("thirdSection");
         let popup = document.getElementById("shareSection");
-        btn.classList.toggle("no-display");
-        popup.classList.toggle("display");
-         console.log("Switched Change");
+        let displayPop = document.getElementById("popped")
+        if(window.innerWidth < 700) { 
+            btn.classList.toggle("no-display");
+            popup.classList.toggle("display");
+            
         setTimeout(() => {
             btn.classList.remove("no-display");
-            popup.classList.remove("display");console.log("Initial Design Restored");
+            popup.classList.remove("display");
              }, "5000")
-        
+        }
+        else{
+            displayPop.classList.toggle("show")
+        };
     });
 
